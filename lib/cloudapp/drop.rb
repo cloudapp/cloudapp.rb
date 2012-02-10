9059c1fe86a324a37c35c01b75ca036dfe1e75cb
Larry Marburger <larry@marburger.cc>
2012-02-10 16:18:49 -0500

    Add Drop#display_name

--- lib/cloudapp/drop.rb
+++ lib/cloudapp/drop.rb
@@ -3,5 +3,9 @@ module CloudApp
   class Drop < OpenStruct
     def private?() private == true end
     def public? () !private?       end
+
+    def display_name
+      name || redirect_url || url
+    end
   end
 end
--- spec/cloudapp/drop_spec.rb
+++ spec/cloudapp/drop_spec.rb
@@ -3,6 +3,35 @@ require 'helper'
 require 'cloudapp/drop'
 
 describe CloudApp::Drop do
+  describe '#display_name' do
+    describe 'a drop with a name' do
+      let(:name) { stub :name }
+      subject    { CloudApp::Drop.new name: name }
+
+      it 'is its name' do
+        subject.display_name.should eq(name)
+      end
+    end
+
+    describe 'a drop with a redirect url and no a name' do
+      let(:redirect_url) { stub :redirect_url }
+      subject { CloudApp::Drop.new redirect_url: redirect_url }
+
+      it 'is its redirect url' do
+        subject.display_name.should eq(redirect_url)
+      end
+    end
+
+    describe 'a drop with no redirect url and name' do
+      let(:url) { stub :url }
+      subject   { CloudApp::Drop.new url: url }
+
+      it 'is its remote url' do
+        subject.display_name.should eq(url)
+      end
+    end
+  end
+
   describe '#private?' do
     describe 'a private drop' do
       subject { CloudApp::Drop.new private: true }
