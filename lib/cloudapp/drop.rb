ae7e9931d5e3dd18a3d410689fa0807a29826a13
Larry Marburger <larry@marburger.cc>
2012-02-23 17:26:32 -0500

    Whitespace

--- lib/cloudapp/drop.rb
+++ lib/cloudapp/drop.rb
@@ -3,7 +3,7 @@ require 'ostruct'
 module CloudApp
   class Drop < OpenStruct
     def private?() private == true end
-    def public? () !private?       end
+    def public?() !private? end
 
     def display_name
       name || redirect_url || url
