e5c28848f51686d1d018857e54c040b2fa31f6db
Larry Marburger <larry@marburger.cc>
2012-05-10 17:11:43 -0400

    Include tracer module in stubbed classes/modules

--- spec/support/stub_class_or_module.rb
+++ spec/support/stub_class_or_module.rb
@@ -1,3 +1,5 @@
+module Stubbed end
+
 def stub_module(full_name, &block)
   stub_class_or_module full_name, Module, &block
 end
@@ -7,13 +9,14 @@ def stub_class(full_name, &block)
 end
 
 def stub_class_or_module(full_name, kind, &block)
-  full_name.to_s.split(/::/).inject(Object) do |context, name|
+  stub = full_name.to_s.split(/::/).inject(Object) do |context, name|
     begin
       context.const_get(name)
     rescue NameError
-      context.const_set(name, kind.new)
+      stubbed = kind.new do include(Stubbed) end
+      context.const_set(name, stubbed)
     end
   end
 
-  Object.const_get(full_name).class_eval(&block) if block
+  stub.class_eval &block if block
 end
