5fbbe9fcab4cf1984d303c3e7186533fbc9aaf50
Larry Marburger <larry@marburger.cc>
2012-08-17 13:54:17 -0400

    Clean up vcr config

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -1,22 +1,22 @@
 require 'vcr'
 
-VCR.configure do |c|
-  c.cassette_library_dir     = 'spec/cassettes'
-  c.default_cassette_options = { record: :none,
-                                 match_requests_on: [ :method, :uri, :body ]}
-  c.hook_into :webmock
-  c.configure_rspec_metadata!
+VCR.configure do |vcr|
+  vcr.cassette_library_dir     = 'spec/cassettes'
+  vcr.default_cassette_options = { record: :once,
+                                   match_requests_on: [ :method, :uri, :body ]}
+  vcr.hook_into :webmock
+  vcr.configure_rspec_metadata!
 end
 
-RSpec.configure do |c|
-  c.treat_symbols_as_metadata_keys_with_true_values = true
-  c.before :each, integration: true do
+RSpec.configure do |rspec|
+  rspec.treat_symbols_as_metadata_keys_with_true_values = true
+  rspec.before :each, integration: true do
     VCR.configure do |vcr|
       vcr.allow_http_connections_when_no_cassette = true
     end
   end
 
-  c.after :each, integration: true do
+  rspec.after :each, integration: true do
     VCR.configure do |vcr|
       vcr.allow_http_connections_when_no_cassette = false
     end
