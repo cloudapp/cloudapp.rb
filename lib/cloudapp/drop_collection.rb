13e290a788bc22e89ceae6eff178873db043d911
Larry Marburger <larry@marburger.cc>
2012-08-24 09:06:07 -0400

    Add DropCollection#follow and #has_link?

--- lib/cloudapp/drop_collection.rb
+++ lib/cloudapp/drop_collection.rb
@@ -4,7 +4,9 @@ require 'forwardable'
 module CloudApp
   class DropCollection < SimpleDelegator
     extend Forwardable
-    def_delegators :@representation, :authorized?, :unauthorized?, :link, :links
+    def_delegators :representation, :authorized?, :unauthorized?#, :link, :links
+
+    attr_reader :representation, :drop_class
 
     def initialize(representation, drop_class = Drop)
       @representation = representation
@@ -12,11 +14,19 @@ module CloudApp
       super drops
     end
 
+    def follow(relation)
+      self.class.new representation.link(relation).follow, drop_class
+    end
+
+    def has_link?(relation)
+      !representation.link(relation) { nil }.nil?
+    end
+
   protected
 
     def drops
       return [] if unauthorized?
-      @drops ||= @representation.items.map {|drop| @drop_class.new(drop) }
+      @drops ||= representation.items.map {|drop| drop_class.new(drop) }
     end
   end
 end
--- spec/cloudapp/drop_collection_spec.rb
+++ spec/cloudapp/drop_collection_spec.rb
@@ -4,12 +4,24 @@ require 'ostruct'
 require 'cloudapp/drop_collection'
 
 describe CloudApp::DropCollection do
-  let(:representation) { stub :representation, unauthorized?: unauthorized,
-                                               items: items }
-  let(:unauthorized)   { nil }
+  let(:representation) { stub :representation, unauthorized?: false,
+                                               items:         items }
   let(:items)          {[ stub(:item1), stub(:item2) ]}
-  let(:drop_class)     { stub(:drop_class, new: nil) }
+  let(:drop_class)     { stub(:drop_class, new: :drop) }
   subject { CloudApp::DropCollection.new representation, drop_class }
+
+  describe 'collection' do
+    it 'is a collection of drops' do
+      subject.size.should eq(2)
+    end
+
+    it 'decodes each drop' do
+      items.each do |drop|
+        drop_class.should_receive(:new).with(drop)
+      end
+      subject
+    end
+  end
 
   describe '#authorized?' do
     let(:authorized) { stub :authorized }
@@ -22,41 +34,63 @@ describe CloudApp::DropCollection do
 
   describe '#unauthorized?' do
     let(:unauthorized) { stub :unauthorized }
+    before do representation.stub(unauthorized?: unauthorized) end
 
     it 'delegates to representation' do
       subject.unauthorized?.should eq(unauthorized)
     end
   end
 
-  describe '#link' do
-    it 'delegates to representation' do
-      args = stub :args
-      representation.should_receive(:link).with(args)
-      subject.link(args)
+  describe '#follow' do
+    let(:link_representation) {
+      stub :link_representation, unauthorized?: false,
+                                 items:         []
+    }
+    subject {
+      CloudApp::DropCollection.new(representation, drop_class).follow('yes')
+    }
+
+    before do
+      link = stub :link
+      link.should_receive(:follow).once.and_return(link_representation)
+      representation.should_receive(:link).once.and_return(link)
     end
+
+    it { subject.should be_a(CloudApp::DropCollection) }
+    its(:representation) { should eq(link_representation) }
+    its(:drop_class)     { should eq(drop_class) }
   end
 
-  describe '#links' do
-    it 'delegates to representation' do
-      args = stub :args
-      representation.should_receive(:links).with(args)
-      subject.links(args)
-    end
-  end
+  describe '#has_link?' do
+    context 'with link' do
+      before do
+        representation.should_receive(:link).
+          once.
+          with('yes').
+          and_return(stub(:link))
+      end
 
-  it 'is a collection of drops' do
-    subject.size.should eq(2)
-  end
-
-  it 'decodes each drop' do
-    items.each do |drop|
-      drop_class.should_receive(:new).with(drop)
+      it 'returns false' do
+        subject.has_link?('yes').should eq(true)
+      end
     end
-    subject
+
+    context 'without link' do
+      before do
+        representation.should_receive(:link).
+          once.
+          with('no').
+          and_yield
+      end
+
+      it 'returns true' do
+        subject.has_link?('no').should eq(false)
+      end
+    end
   end
 
   context 'unauthorized' do
-    let(:unauthorized) { stub :unauthorized }
+    before do representation.stub(unauthorized?: true) end
 
     it { should be_empty }
   end
